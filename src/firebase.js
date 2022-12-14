4fd399bd0cc4dceaaa320353564c50bb518e0e54
teanmi <teanmi6@gmail.com>
2022-12-13 19:55:15 -0500

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "twitter-clone-ea158.firebaseapp.com",
+  projectId: "twitter-clone-ea158",
+  storageBucket: "twitter-clone-ea158.appspot.com",
+  messagingSenderId: "233019399841",
+  appId: "1:233019399841:web:e247a851d85f9049c4dae0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
